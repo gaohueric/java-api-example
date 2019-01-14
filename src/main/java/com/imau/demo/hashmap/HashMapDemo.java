c2d2386d510bd77a4614f36abc53ca5dc61c6a58
高虎 <gh43690@ly.com>
2019-01-14 18:13:02 +0800

    fix

---
+++ src/main/java/com/imau/demo/hashmap/HashMapDemo.java
@@ -0,0 +1,17 @@
+
+package com.imau.demo.hashmap;
+
+import java.util.HashMap;
+
+/**
+ * @author gh43690
+ * @create $ ID: HashMapDemo, 2019/1/14 14:29 gh43690 Exp $
+ * @since 1.0.0
+ */
+public class HashMapDemo  {
+
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<String, String>();
+
+    }
+}
